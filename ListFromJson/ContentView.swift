15510301e31d49293b9820f61e458aff4c4b46b5
Brent Schooley <schooleybrent@gmail.com>
2019-06-28 17:37:41 -0400

    Initial Commit

---
+++ ListFromJson/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ListFromJson
+//
+//  Created by Brent Schooley on 6/28/19.
+//  Copyright © 2019 Brent Schooley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
